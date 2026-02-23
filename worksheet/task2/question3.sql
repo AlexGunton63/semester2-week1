d171d7f1a30b16107e2ff667f9b6798d574dc71d
AlexGunton63 <kbzz0907@leeds.ac.uk>
2026-02-23 20:00:06 +0000

    Fixed grouping issue

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -4,4 +4,4 @@
 
 --SELECT DepartmentName, Count(*) AS TotalEnrolments FROM Department GROUP BY DepartmentName 
 
-SELECT Department.DepartmentName, COUNT(Enrolment.EnrolmentId) AS TotalEnrolments FROM Department JOIN Course ON Department.DepartmentId = Course.DepartmentId JOIN Enrolment ON Course.CourseId = Enrolment.CourseId 
+SELECT Department.DepartmentName, COUNT(Enrolment.EnrolmentId) AS TotalEnrolments FROM Department JOIN Course ON Department.DepartmentId = Course.DepartmentId JOIN Enrolment ON Course.CourseId = Enrolment.CourseId GROUP BY Department.DepartmentName
